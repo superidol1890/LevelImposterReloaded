5b6de19ca50a4847b112165c4bac0604c642c47c
Super Idol <viyanshkakkar@gmail.com>
2025-11-29 20:01:25 +0530

    Add files via upload
    Minor Code Fix

---
+++ LevelImposter/Builders/TransformBuilder.cs
@@ -0,0 +1,26 @@
+﻿using LevelImposter.Core;
+using UnityEngine;
+
+namespace LevelImposter.Builders;
+
+/// <summary>
+///     Configures the Transform on the GameObject
+/// </summary>
+public class TransformBuilder : IElemBuilder
+{
+    public void OnPreBuild(LIElement elem, GameObject obj)
+    {
+        obj.layer = (int)Layer.Ship;
+        obj.transform.localPosition = new Vector3(elem.x, elem.y, elem.z);
+        obj.transform.localRotation = Quaternion.Euler(0, 0, elem.rotation);
+        obj.transform.localScale = new Vector3(elem.xScale, elem.yScale, 1.0f);
+    }
+
+    public void OnBuild(LIElement elem, GameObject obj)
+    {
+        // Scale Z position by Y if not a util-layer
+        // Layers will mess up the Z position
+        if (elem.type != "util-layer")
+            obj.transform.position = MapUtils.ScaleZPositionByY(obj.transform.position);
+    }
+}
